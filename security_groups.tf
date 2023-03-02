ad989c275da22f9601f3f0a4ac3317ef83969b34
Elias Lopez <eliasjavier@gmail.com>
2023-03-02 22:07:17 +0000

    adding security groups

---
+++ security_groups.tf
@@ -0,0 +1,118 @@
+# Security group for elb
+resource "aws_security_group" "my_webapp_elb_sg" {
+
+  vpc_id = aws_vpc.my_webapp.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-webapp-elb-sg"
+  }
+}
+
+# Security group for bastion host
+resource "aws_security_group" "my_webapp_bastion_sg" {
+
+  vpc_id = aws_vpc.my_webapp.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-webapp-bastion-sg"
+  }
+}
+
+
+
+# Security groups for the app
+resource "aws_security_group" "my_webapp_app_sg" {
+
+  vpc_id = aws_vpc.my_webapp.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.my_webapp_elb_sg.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.my_webapp_bastion_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-webapp-app-sg"
+  }
+}
+
+# Service groups for the db
+resource "aws_security_group" "my_webapp_db_sg" {
+
+  vpc_id = aws_vpc.my_webapp.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.my_webapp_app_sg.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.my_webapp_bastion_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-webapp-db-sg"
+  }
+}
